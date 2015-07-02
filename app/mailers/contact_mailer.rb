6c1ab492a47d754d132f93b2f96326cad90b37bd
Ross <rossnovick@pacbell.net>
2015-07-02 18:05:46 +0000

    Correction to contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@ class ContactMailer < ActionMailer::Base
   def contact_email(name, email, body)
     @name = name
     @email = email
-    @bady = body
+    @body = body
     mail(from: email, subject: 'Contact Form Message')
   end
 end
