74db3407e4bb239dc2143cf2378471e54d0d4e93
Ross Novick <rossnovick@pacbell.net>
2015-08-01 01:32:42 +0000

    Added profile model and table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,13 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   belongs_to :plan
+  has_one :profile
   attr_accessor :stripe_card_token
   def save_with_payment
     if valid?
       customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
-    else
-      flash[:notice] = "input is not valid"
     end
   end
 end
